05be8c5a2aacc45b48bb5b42819c307a7aeb1adf
bumpy <62362511+BumpyBill@users.noreply.github.com>
2022-05-15 13:09:00 +1000

    Update hello_world.rs

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -11,7 +11,7 @@ fn main() {
     });
 
     match res {
-        Ok(()) => println!("exited succesfully!"),
+        Ok(()) => println!("exited successfully!"),
         Err(e) => panic!("{}", e),
     }
 }
